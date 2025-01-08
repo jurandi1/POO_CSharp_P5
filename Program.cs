083a53e4cf965321585d2ef8eb25365750916a55
Jurandi Junior <jurandijr20@gmail.com>
2025-01-08 18:32:41 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace POO_CSharp_P5
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
