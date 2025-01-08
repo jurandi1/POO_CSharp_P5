eccafdebdd264ffb84cf31f400401a7d4b29d7cb
Jurandi Junior <jurandijr20@gmail.com>
2025-01-08 19:18:41 -0300

    Nullable e Operador de coalescência nula

--- Program.cs
+++ Program.cs
@@ -8,6 +8,46 @@ namespace POO_CSharp_P5
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.WriteLine("Operador de coalescência nula");
+            double? x = null;
+            double? y = 10.0;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine();
+            Console.WriteLine("Nullable");
+             x = null;
+             y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Tipos referência vs.tipos valor");
             //Tipos referência vs.tipos valor
             //CLASSE
             //Vantagem: usufrui de todos recursos OO
